8fcb1a9dd6cf2badc3820a6bef361335827bad47
Nicholas Mendez <mendez.nicholasr@gmail.com>
2022-05-16 13:27:57 -0400

    completed

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,21 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    count = self.split(/[!.?]{1}/)
+    result = count.filter {|word| word.length > 1}
+    result.length
   end
 end
+
